7a10310ccfdf205cf215ed174f5618376954ce67
michaelstsnow <michael.st.snow@gmail.com>
2018-11-17 21:44:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.match(/[.]$/)
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.match(/[?]$/)
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.match(/[!]$/)
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
-  end
+    final_segments=[]
+    self.split(/\.|\?|\!/).each do |segment|
+      if segment.empty?
+      else
+        final_segments << segment
+      end #end of If statement
+    end # end of do
+    final_segments.size
+  end # end of def
 end
